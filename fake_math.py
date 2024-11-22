504dddde1d88f8effe19f3504b09c75efaa9e48b
yanmazur <zacheper102@gmail.com>
2024-11-22 13:05:35 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(first,second):
+    if second == 0:
+        return "error"
+    else:
+        return first/second
---
+++ midule_4_1.py
@@ -0,0 +1,16 @@
+import fake_math as a
+import true_math as b
+
+
+fake_divide = a.divide
+true_divide = b.divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
